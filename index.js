30782b8732c077a744271d5a48188fbf4df57906
FOYJUR RAHMAN <mdjisaun86@gmail.com>
2026-04-30 01:12:49 +0600

    add gitignore

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hey developer");
+const express = require("express")
+const app = express()
+
+app.get('/', (req,res)=>{
+    res.send("hello")
+})
+
+app.listen(5000,()=>{
+    console.log("server runnig on port 5000");
+
+})
